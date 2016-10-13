6272f6be4b45b7853fbcb24b7c0175e89295a27c
= <=>
2016-10-13 10:55:05 -0300

    Estructura TP2, la idea es como el tp 1 armar un menu fuera del controloador y que este llame a los metodos. En librerias esta el mapreduce que debemos usar y en configs las fuentes rss que usamos. Hay que hacer el lector de rss cada 10 minutos que levante el xml, lo guarde y luego lo una al xml nuestro con nuestra estructura del tp, es hacer un xml con una estructura que nos ayude para la parte dos del tp.

---
+++ config/rss_sources.py
@@ -0,0 +1,13 @@
+rss_sources = {
+    #TELAM RSS
+    "telam": {
+        "ultimas": "http://www.telam.com.ar/rss2/ultimasnoticias.xml",
+        "politica": "http://www.telam.com.ar/rss2/politica.xml",
+        "sociedad": "http://www.telam.com.ar/rss2/sociedad.xml",
+        "economia": "http://www.telam.com.ar/rss2/economia.xml",
+        "mundo": "http://www.telam.com.ar/rss2/mundo.xml"
+    }
+
+    #
+}
+
