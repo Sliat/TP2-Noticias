acf347093c7161cd9dc453082309772cc9aacdd0
Oscar Andrés Argueyo <ozz.rokr@hotmail.com>
2016-10-17 22:39:27 -0300

    se agrego mas resources de medios al diccionario

--- config/rss_sources.py
+++ config/rss_sources.py
@@ -6,8 +6,31 @@ rss_sources = {
         "sociedad": "http://www.telam.com.ar/rss2/sociedad.xml",
         "economia": "http://www.telam.com.ar/rss2/economia.xml",
         "mundo": "http://www.telam.com.ar/rss2/mundo.xml"
+    },
+
+    "clarin":{
+        "ultimas": "http://www.clarin.com/rss/lo-ultimo/",
+        "politica": "http://www.clarin.com/rss/politica/",
+        "sociedad": "http://www.clarin.com/rss/sociedad/",
+        "economia": "http://www.clarin.com/rss/ieco/",
+        "mundo": "http://www.clarin.com/rss/mundo/"
+    },
+
+    "bbc":{
+        "ultimas": "http://www.bbc.co.uk/mundo/ultimas_noticias/index.xml",
+        "america-latina": "http://www.bbc.co.uk/mundo/temas/america_latina/index.xml",
+        "cultura": "http://www.bbc.co.uk/mundo/temas/cultura/index.xml",
+        "economia": "http://www.bbc.co.uk/mundo/temas/economia/index.xml",
+        "mundo": "http://www.bbc.co.uk/mundo/temas/internacional/index.xml"
+    },
+
+    "la-nacion":{
+        "ultimas": "http://contenidos.lanacion.com.ar/herramientas/rss/origen=2 ",
+        "politica": "http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=30 ",
+        "sociedad": "http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=7773 ",
+        "economia": "http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=272 ",
+        "mundo": "http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=7 "
     }
 
-    #
 }
 
