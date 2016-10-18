ac129124d91ce5e74131c7ca2043797fabe85050
Oscar Andrés Argueyo <=>
2016-10-18 16:19:17 -0300

    Generar dinamicamente los xml de los medios para el indice invertido  con las secciones como sub-elementos con name como la key del dic. Empezar la extraccion de noticias nuevas

--- config/rss_sources.py
+++ config/rss_sources.py
@@ -1,42 +1,45 @@
 rss_sources = {
-    #TELAM RSS
     "telam":{
+        'nombre': "Telam",
         'secciones':{
-            "ultimas": "http://www.telam.com.ar/rss2/ultimasnoticias.xml",
-            "politica": "http://www.telam.com.ar/rss2/politica.xml",
-            "sociedad": "http://www.telam.com.ar/rss2/sociedad.xml",
-            "economia": "http://www.telam.com.ar/rss2/economia.xml",
-            "mundo": "http://www.telam.com.ar/rss2/mundo.xml"
+            "ultimas"   : "http://www.telam.com.ar/rss2/ultimasnoticias.xml",
+            "politica"  : "http://www.telam.com.ar/rss2/politica.xml",
+            "sociedad"  : "http://www.telam.com.ar/rss2/sociedad.xml",
+            "economia"  : "http://www.telam.com.ar/rss2/economia.xml",
+            "mundo"     : "http://www.telam.com.ar/rss2/mundo.xml"
         }
     },
 
     "clarin":{
+        'nombre': "Clarin",
         'secciones':{
-            "ultimas": "http://www.clarin.com/rss/lo-ultimo/",
-            "politica": "http://www.clarin.com/rss/politica/",
-            "sociedad": "http://www.clarin.com/rss/sociedad/",
-            "economia": "http://www.clarin.com/rss/ieco/",
-            "mundo": "http://www.clarin.com/rss/mundo/"
+            "ultimas"   : "http://www.clarin.com/rss/lo-ultimo/",
+            "politica"  : "http://www.clarin.com/rss/politica/",
+            "sociedad"  : "http://www.clarin.com/rss/sociedad/",
+            "economia"  : "http://www.clarin.com/rss/ieco/",
+            "mundo"     : "http://www.clarin.com/rss/mundo/"
         }
     },
 
     "bbc":{
+        'nombre': "BBC en Español",
         'secciones':{
-            "ultimas": "http://www.bbc.co.uk/mundo/ultimas_noticias/index.xml",
+            "ultimas"       : "http://www.bbc.co.uk/mundo/ultimas_noticias/index.xml",
             "america-latina": "http://www.bbc.co.uk/mundo/temas/america_latina/index.xml",
-            "cultura": "http://www.bbc.co.uk/mundo/temas/cultura/index.xml",
-            "economia": "http://www.bbc.co.uk/mundo/temas/economia/index.xml",
-            "mundo": "http://www.bbc.co.uk/mundo/temas/internacional/index.xml"
+            "cultura"       : "http://www.bbc.co.uk/mundo/temas/cultura/index.xml",
+            "economia"      : "http://www.bbc.co.uk/mundo/temas/economia/index.xml",
+            "mundo"         : "http://www.bbc.co.uk/mundo/temas/internacional/index.xml"
         }
     },
 
-    "lanacion":{
-        'secciones':{
-            "ultimas": "http://contenidos.lanacion.com.ar/herramientas/rss/origen=2 ",
-            "politica": "http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=30 ",
-            "sociedad": "http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=7773 ",
-            "economia": "http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=272 ",
-            "mundo": "http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=7 "
+    "lanacion": {
+        'nombre': "La Nación",
+        'secciones': {
+            "ultimas"   : "http://contenidos.lanacion.com.ar/herramientas/rss/origen=2 ",
+            "politica"  : "http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=30 ",
+            "sociedad"  : "http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=7773 ",
+            "economia"  : "http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=272 ",
+            "mundo"     : "http://contenidos.lanacion.com.ar/herramientas/rss/categoria_id=7 "
         }
     }
 }
--- modelos/Medio.py
+++ modelos/Medio.py
@@ -6,12 +6,37 @@
         extraer_[clave del dicc en config/rss_sources]
 
 '''
-
+import config.rss_sources as sources
+import os
+import urllib.request
+from lxml import etree
 
 class Medio(object):
+    medios = sources.rss_sources
+    sources_path = os.path.join(os.path.dirname(__file__),"..","sources")
+    def __init__(self):
+        #no hace falta bajar los rss podemos trabajarlos directamente en la generacion de
+        #nuestro propio xml
+        self._check_medios()
+
+    def _check_medios(self):
+        for idmedio in sorted(sources.rss_sources.keys()):
+            try:
+                archivo_xml = os.path.join(self.sources_path, idmedio+".xml")
+                if not os.path.isfile(archivo_xml):
+                   self.create_medio_xml(idmedio , archivo_xml)
+            except OSError as exception:
+                if exception.errno != errno.EEXIST:
+                    raise
 
     def extraer_telam(self):
-        print("extraigo de telam")
+        telam_rss = sources.rss_sources["telam"]["secciones"]
+        for id,url_seccion in telam_rss.items():
+            tree = etree.parse(self.extraer_rss(url_seccion))
+            print(tree)
+            exit()
+
+
 
     def extraer_clarin(self):
         print("extraigo de clarin")
@@ -21,3 +46,29 @@ class Medio(object):
 
     def extraer_bbc(self):
         print("extraigo de bbc")
+
+    def extraer_rss(self, url):
+        try:
+            req = urllib.request.Request(url)
+            with urllib.request.urlopen(req) as rss:
+                datos = rss.read()
+            return datos
+        except:
+            return None
+
+    def create_medio_xml(self , idmedio, archivo_xml):
+        root = etree.Element('medio')
+        root.set("id" , idmedio)
+        doc = etree.ElementTree(root)
+        nombre = etree.SubElement(root , "nombre")
+        nombre.text = self.medios[idmedio]["nombre"]
+
+        for idseccion in sorted(self.medios[idmedio]["secciones"].keys()):
+            etree.SubElement(root, 'seccion', name=idseccion)
+
+        doc.write(archivo_xml, pretty_print=True, xml_declaration=True, encoding='UTF-16')
+
+
+if __name__ == "__main__":
+    medios = Medio()
+    #medios.extraer_telam()
